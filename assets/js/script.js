5e1af82a7a30e6f62be2a2e876e105634d49a61b
Fernando Santacruz <santacruzfernandog@gmail.com>
2020-11-23 19:59:13 -0300

    Sitio completo convertido a una One Page

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Iniciar Scrollspy
+$('body').scrollspy({target: '#menu-navegacion'})
+
+//Scroll suavizado
+$('#menu-navegacion a').on('click', function(event){
+    if(this.hash != ""){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+            window.location.hash = hash;
+        })
+    }
+})
