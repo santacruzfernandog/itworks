fb8bf523b1ba2a4903045edec2a6312cc47b62ea
Fernando Santacruz <santacruzfernandog@gmail.com>
2020-12-02 22:10:36 -0300

    Arreglo principal: responsive boton hamburguesa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
-// Iniciar Scrollspy
-$('body').scrollspy({target: '#menu-navegacion'})
+$(document).ready(function(){
+    $('body').scrollspy({target: '#menu-navegacion'})
 
-//Scroll suavizado
-$('#menu-navegacion a').on('click', function(event){
-    if(this.hash != ""){
-        event.preventDefault();
-        const hash = this.hash;
+    //Scroll suavizado
+    $('#menu-navegacion a').on('click', function(event){
+        if(this.hash != ""){
+            event.preventDefault(event);
+            const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-            window.location.hash = hash;
-        })
-    }
-})
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            })
+        }
+    })
+});
